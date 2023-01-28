e3fde98b4b670d969f2eb003825d53443cd4dbdb
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-01-28 13:27:55 +0600

    Add files via upload

---
+++ Tram.cpp
@@ -0,0 +1,87 @@
+///Tram
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 100
+#define infinity 1000000000000000LL
+#define all(x) (x).begin(),(x).end()
+#define FastRead ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define mod(x,m) ((x%m)+m)%m;                               //101rror
+#define fastread() (ios_base:: sync_with_stdio(false),cin.tie(NULL))
+
+#define T while(t--)
+#define YES             cout << "YES";
+#define NO              cout << "NO";
+#define Yes             cout << "Yes";
+#define No              cout << "No";
+#define OK              cout << "OK";
+#define ERROR           cout << "ERROR";
+#define Ok              cout << "Ok";
+#define Error           cout << "Error";
+#define nl              cout << endl;
+#define spa             cout << " ";
+
+#define fi first
+#define se second
+#define ll long long
+#define vc vector<char>
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define pop pop_back
+#define sc set<char>
+#define si set<int>
+#define sll set<ll>
+#define sin st.insert
+#define scl(st) st.clear()
+#define eb emplace_back
+#define all(x) (x).begin(),(x).end()
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    fastread();
+
+    int t;
+    cin >> t;
+
+    int sum = 0;
+
+    vi vec;
+
+    while(t--)
+    {
+        int a, b;
+
+        cin >> a >> b;
+
+        sum += b;
+        sum -= a;
+
+        vec.pb(sum);
+    }
+
+    sort(vec.begin(), vec.end(), greater<int>());
+
+    cout << vec.front();
+
+
+
+    return 0;
+}
