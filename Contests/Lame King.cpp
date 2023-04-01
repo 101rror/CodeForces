064e18491c9bc29626462bc1114a3c6b23a13a75
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-04-02 00:57:13 +0600

    Add files via upload

---
+++ Contests/Lame King.cpp
@@ -0,0 +1,89 @@
+///Lame King
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+const int N = 1e6+10;
+#define MAX 1000000                                     //101rror
+#define FastRead() (ios_base:: sync_with_stdio(false),cin.tie(0))
+
+#define  YES      cout << "YES";
+#define  NO       cout << "NO";
+#define  Yes      cout << "Yes";
+#define  No       cout << "No";
+#define  one      cout << "-1";
+#define  nl       cout << "\n";
+
+#define  srt(s)   sort(s.begin(),s.end());
+#define  rsrt(s)  sort(s.rbegin(),s.rend());
+
+#define  signed   int32_t
+#define  long     long long
+#define  fi       first
+#define  se       second
+#define  pb       push_back
+#define  pp       pop_back
+
+
+
+void passion()
+{
+    long a, b;
+    cin >> a >> b;
+
+    long ans = 0;
+
+    if(a < 0)
+    {
+        a *= -1;
+    }
+    if(b < 0)
+    {
+        b *= -1;
+    }
+    int mn = min(a, b);
+
+    ans += 2 * mn;
+
+    a -= mn;
+    b -= mn;
+
+    int mx = max(a, b);
+
+    if (mx > 0)
+    {
+        ans +=2 * mx;
+        ans--;
+    }
+
+    cout << ans;
+    nl
+
+}
+
+
+
+int main()
+{
+    FastRead();
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    long testCase;
+    cin >> testCase;
+
+    while(testCase--)
+    {
+        passion();
+    }
+
+    return 0;
+}
