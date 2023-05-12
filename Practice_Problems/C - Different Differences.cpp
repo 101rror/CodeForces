2b34b04e22e1ba739f4a348486bb4e499013070e
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-05-12 23:55:34 +0600

    Add files via upload

---
+++ Practice_Problems/C - Different Differences.cpp
@@ -0,0 +1,83 @@
+///Different Differences
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e6+10;
+#define MAX 1000000
+#define infinity 1000000000000000LL                             //101rror
+#define fastRead() (ios_base:: sync_with_stdio(false),cin.tie(0))
+
+#define YES   cout << "YES";
+#define NO    cout << "NO";
+#define Yes   cout << "Yes";
+#define No    cout << "No";
+#define one   cout << "-1";
+#define nl    cout << endl;
+
+#define int32_t signed
+#define int long long
+#define fi first
+#define se second
+#define vc vector<char>
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define pop pop_back
+#define sc set<char>
+#define si set<int>
+#define sll set<ll>
+#define sin st.insert
+#define scl(st) st.clear()
+#define eb emplace_back
+#define all(x) (x).begin(),(x).end()
+
+#ifdef LOCAL
+#include "algo/debug.h"
+#else
+#define debug(...) 42
+#endif
+
+
+
+void solve()
+{
+    int k, n;
+    cin >> k >> n;
+
+    int array[k];
+
+    for(int i = 0; i < k; i++)
+    {
+        int maximum = i + 1 + min(n - k, i * (i - 1) / 2);
+
+        cout << maximum << " ";
+    }
+    nl
+}
+
+
+
+signed main()
+{
+    fastRead();
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--)
+    {
+        solve();
+    }
+
+    return 0;
+}
