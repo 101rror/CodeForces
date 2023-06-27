fd98e0f8bf3a7fa541bd2974391a6dbb16b06fb0
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-06-27 23:35:24 +0600

    Add files via upload

---
+++ Practice_Problems/B - Team Composition, Programmers and Mathematicians.cpp
@@ -0,0 +1,67 @@
+///Team Composition: Programmers and Mathematicians
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+const int N = 1e6+10;
+#define MAX 1000000                                     //101rror
+#define FastRead() (ios_base:: sync_with_stdio(false),cin.tie(0))
+
+#define  YES   cout << "YES";
+#define  NO    cout << "NO";
+#define  Yes   cout << "Yes";
+#define  No    cout << "No";
+#define  one   cout << "-1";
+#define  nl    cout << "\n";
+
+#define  signed  int32_t
+#define  int  long long
+#define  fi  first
+#define  se  second
+#define  vc  vector<char>
+#define  vi  vector<int>
+#define  pb  push_back
+#define  pop  pop_back
+#define  sc  set<char>
+#define  si  set<int>
+
+
+
+void solve()
+{
+    pair<int, int>team;
+
+    cin >> team.fi >> team.se;
+
+    int count = min(min(team.fi, team.se), (team.fi + team.se) / 4);
+
+    cout << count;
+    nl
+}
+
+
+
+signed main()
+{
+    FastRead();
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--)
+    {
+        solve();
+    }
+
+    return 0;
+}
