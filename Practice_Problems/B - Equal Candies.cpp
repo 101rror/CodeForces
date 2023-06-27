e026876ae956250df48e037a422922f175fb08f9
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-06-27 23:45:10 +0600

    Add files via upload

---
+++ Practice_Problems/B - Equal Candies.cpp
@@ -0,0 +1,82 @@
+///Equal Candies
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+const int N = 1e6+10;
+#define MAX 1000000                                     //101rror
+#define FastRead() (ios_base:: sync_with_stdio(false),cin.tie(0))
+
+#define  YES      cout << "YES";
+#define  NO       cout << "NO";
+#define  Yes      cout << "Yes";
+#define  No       cout << "No";
+#define  one      cout << "-1";
+#define  nl       cout << "\n";
+
+#define  srt(s)   sort(s.begin(),s.end())
+#define  rsrt(s)  sort(s.rbegin(),s.rend())
+
+#define  signed   int32_t
+#define  long     long long
+#define  fi       first
+#define  se       second
+#define  pb       push_back
+#define  pp       pop_back
+
+
+
+void passion()
+{
+    long n;
+    cin >> n;
+
+    vector<int>vec;
+
+    int arr[n];
+    int sum = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    int x = arr[0];
+
+    int candies = (x * n);
+
+    int ans = (sum - candies);
+
+    cout << ans;
+    nl
+}
+
+
+
+int main()
+{
+    FastRead();
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    long testCase;
+    cin >> testCase;
+
+    while(testCase--)
+    {
+        passion();
+    }
+
+    return 0;
+}
