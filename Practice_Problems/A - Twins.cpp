dee4303a7d04bc2062463b4c9d00107badd66eca
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-09-12 22:38:58 +0600

    Add files via upload

---
+++ Practice_Problems/A - Twins.cpp
@@ -0,0 +1,97 @@
+///Twins
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+const int N = 1e6+10;
+#define MAX 1000000                                     //101rror
+#define FastRead() (ios_base:: sync_with_stdio(false),cin.tie(0))
+
+#define  YES      cout << "YES";
+#define  NO       cout << "NO";
+#define  Yes      cout << "Yes";
+#define  No       cout << "No";
+#define  one      cout << "-1";
+#define  nl       cout << "\n";
+
+#define  srt(s)   sort(s.begin(),s.end());
+#define  rsrt(s)  sort(s.rbegin(),s.rend());
+
+#define  signed   int32_t
+#define  long     long long
+#define  fi       first
+#define  se       second
+#define  pb       push_back
+#define  pp       pop_back
+
+
+
+void passion()
+{
+    int n;
+    cin >> n;
+
+    vector<int>s1(n);
+
+    int sum1 = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> s1[i];
+
+        sum1 += s1[i];
+    }
+
+    int sum = sum1;
+
+    vector<int>s(s1);
+
+    srt(s)
+
+    int x = (sum1 / 2);
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+
+        sum -= s[i];
+
+        if(sum > x)
+        {
+            count++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    int ans = (n - count);
+
+    cout << ans;
+    nl
+}
+
+
+
+int main()
+{
+    FastRead();
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+
+    passion();
+
+
+    return 0;
+}
