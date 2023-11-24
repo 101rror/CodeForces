ab4d26b84dec04ffb1a2cb49446d284cda9500c6
Mr. Error <122160056+101rror@users.noreply.github.com>
2023-11-24 12:37:00 +0600

    Add files via upload

---
+++ Contests/CF Round 909 (Div 3)/A - Game with Integers.cpp
@@ -0,0 +1,87 @@
+
+///(لَا إِلَٰهَ إِلَّا ٱللَّٰهُ مُحَمَّدٌ رَّسُولُ ٱللَّٰهِ‎)
+///There is none worthy of worship except ALLAH and Muhammad(SWT) is the messenger of ALLAH.
+
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define FastRead    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define SuccessfullyExecuted return 0;
+
+#define  YES       printf("YES\n");
+#define  NO        printf("NO\n");
+#define  Yes       printf("Yes\n");
+#define  No        printf("No\n");
+#define  one       printf("1\n");
+#define  mone      printf("-1\n");
+#define  zero      printf("0\n");
+#define  nl        printf("\n");
+
+#define  asrt(s)   sort(s, s + n);
+#define  arsrt(s)  sort(s, s + n, greater<long>());
+#define  arvs(s)   reverse(s, s + n);
+#define  srt(s)    sort(s.begin(),s.end());
+#define  rsrt(s)   sort(s.rbegin(),s.rend());
+#define  rvs(s)    reverse(s.begin(), s.end());
+
+#define  long      long long
+#define  fi        first
+#define  se        second
+#define  pb        push_back
+
+
+const int N = 2000001;
+const int MOD = 1000000007;
+long arr[N];
+long brr[N];
+
+
+void passion()
+{
+    long n;
+    scanf("%lld", &n);
+
+    if(n == 1)
+    {
+        printf("First\n");
+    }
+    else if(n % 3 == 0)
+    {
+        printf("Second\n");
+    }
+    else
+    {
+        printf("First\n");
+    }
+}
+
+
+
+int main()
+{
+
+    //FastRead
+
+    long ToS, ToE;
+    scanf("%lld", &ToS);
+
+    ToE = ToS;
+
+    while(ToE--)
+    {
+        passion();
+    }
+
+    //passion();
+
+
+    SuccessfullyExecuted
+}
