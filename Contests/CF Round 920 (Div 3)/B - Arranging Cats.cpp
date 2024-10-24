676c292d86bb2f315489af88782ade69d87ef170
Mr. Error <mr.101rror@gmail.com>
2024-10-24 12:59:47 +0600

    Update

---
+++ Contests/CF Round 920 (Div 3)/B - Arranging Cats.cpp
@@ -0,0 +1,107 @@
+///There is none worthy of worship except ALLAH and Muhammad(SWT) is the messenger of ALLAH.
+
+
+/**
+*
+*   author :: 101rror
+*
+**/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define FastRead    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define SuccessfullyExecuted return 0;
+
+#define  YES       printf("YES\n");
+#define  NO        printf("NO\n");
+#define  Yes       printf("Yes\n");
+#define  No        printf("No\n");
+#define  one       printf("1\n");
+#define  mone      printf("-1\n");
+#define  zero      printf("0\n");
+#define  nl        printf("\n");
+
+#define  asrt(s)   sort(s, s + n);
+#define  arsrt(s)  sort(s, s + n, greater<long>());
+#define  arvs(s)   reverse(s, s + n);
+#define  srt(s)    sort(s.begin(),s.end());
+#define  rsrt(s)   sort(s.rbegin(),s.rend());
+#define  rvs(s)    reverse(s.begin(), s.end());
+
+#define  long      long long
+#define  fi        first
+#define  se        second
+#define  pb        push_back
+
+
+const int N = 2000001;
+const int MOD = 1000000007;
+long arr[N];
+long brr[N];
+
+
+void passion()
+{
+    long n;
+    scanf("%lld", &n);
+    //bool flag = false;
+
+    string s, f;
+    cin >> s;
+    cin >> f;
+
+    if(f == s)
+    {
+        zero
+        return;
+    }
+
+    long o = 0;
+    long z = 0;
+
+    for(long i = 0; i < n; i++)
+    {
+        if((f[i] != s[i]) and s[i] == '1')
+        {
+            o++;
+        }
+        else if((f[i] != s[i]) and s[i] == '0')
+        {
+            z++;
+        }
+    }
+
+    if(o <= z)
+    {
+        cout << z;
+        nl
+    }
+    else
+    {
+        cout << o;
+        nl
+    }
+}
+
+
+
+int main()
+{
+    long ToS, ToE;
+    scanf("%lld", &ToS);
+
+    ToE = ToS;
+
+    while(ToE--)
+    {
+        passion();
+    }
+
+    //passion();
+
+
+    SuccessfullyExecuted
+}
+
